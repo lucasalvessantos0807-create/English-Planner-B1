b848416ff16ca81ced75b11ff207cdf39c586400
lucasalvessantos0807-create <lucas.alves.santos0807@gmail.com>
2026-06-28 14:10:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,11 +31,20 @@ onAuthStateChanged(auth, async (user) => {
         const fontToggle = document.getElementById('fontStyleToggle');
         const fontWrapper = document.getElementById('fontPickerWrapper');
 
-        fontToggle.onclick = () => {
-            const isHidden = fontWrapper.style.display === 'none';
-            fontWrapper.style.display = isHidden ? 'block' : 'none';
-            fontToggle.classList.toggle('expanded', isHidden);
-        };
+        if (fontToggle && fontWrapper) {
+            fontToggle.onclick = (e) => {
+                // Força a detecção do estado atual independente de como foi iniciado
+                const isCurrentlyHidden = window.getComputedStyle(fontWrapper).display === 'none';
+                
+                if (isCurrentlyHidden) {
+                    fontWrapper.style.setProperty('display', 'block', 'important');
+                    fontToggle.classList.add('expanded');
+                } else {
+                    fontWrapper.style.setProperty('display', 'none', 'important');
+                    fontToggle.classList.remove('expanded');
+                }
+            };
+        }
 
         const googleFonts = [
             "Abel", "Abril Fatface", "Aclonica", "Acme", "Actor", "Adamina", "Advent Pro", "Aguafina Script", "Akronim", "Aladin", "Aldrich", "Alef", "Alegreya", "Alex Brush", "Alfa Slab One", "Alice", "Alike", "Allan", "Allerta", "Allura", "Almendra", "Amarante", "Amaranth", "Amatic SC", "Amethysta", "Amiri", "Amita", "Anaheim", "Andada", "Andika", "Angkor", "Annie Use Your Telescope", "Anonymous Pro", "Antic", "Anton", "Arapey", "Arbutus", "Architects Daughter", "Archivo Black", "Are You Serious", "Arial", "Arima Madurai", "Arimo", "Arizonia", "Armata", "Artifika", "Arvo", "Arya", "Asap", "Asar", "Asset", "Assistant", "Astloch", "Asul", "Athiti", "Atma", "Atomic Age", "Aubrey", "Audiowide", "Autour One", "Average", "Averia Libre", "Bangers", "Barlow", "Baskervville", "Bebas Neue", "Belgrano", "Belleza", "BenchNine", "Bentham", "Berkshire Swash", "Bevan", "Bigelow Rules", "Bigshot One", "Bilbo", "BioRhyme", "Biryani", "Bitter", "Black Ops One", "Bokor", "Bonbon", "Boogaloo", "Bowlby One", "Brawler", "Bree Serif", "Bubblegum Sans", "Buda", "Cabin", "Calligraffitti", "Candal", "Cantarell", "Cardo", "Carme", "Caveat", "Chakra Petch", "Changa One", "Charm", "Chivo", "Cinzel", "Comfortaa", "Cookie", "Cormorant", "Courgette", "Crimson Text", "Dancing Script", "Domine", "Doshesis", "Droid Sans", "Eczar", "Exo", "Fahkwang", "Fira Sans", "Frank Ruhl Libre", "Gloria Hallelujah", "Great Vibes", "Heebo", "Hind", "Inconsolata", "Indie Flower", "Inter", "Josefin Sans", "Jost", "Kanit", "Karla", "Lato", "Libre Baskerville", "Lobster", "Lora", "Mali", "Manuscript", "Merriweather", "Montserrat", "Mukta", "Nanum Gothic", "Noto Sans", "Nunito", "Open Sans", "Oswald", "Oxygen", "Pacifico", "PT Sans", "PT Serif", "Playfair Display", "Poppins", "Quicksand", "Raleway", "Roboto", "Rubik", "Saira", "Shadows Into Light", "Slabo 27px", "Source Sans Pro", "Spectral", "Titillium Web", "Ubuntu", "Varela Round", "Work Sans", "Zilla Slab"
